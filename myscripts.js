80ee1b2f58901a4e0ec09d719bda4400f7ced810
D2Gambit <rosenbauma1@gmail.com>
2025-04-20 16:53:16 -0400

    Levi | Fixed the form so the formSubmit is working

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,11 @@
-function submitForm(e) {
-    e.preventDefault();
+function submitForm(event) {
+    event.preventDefault();
     var name = document.getElementById("nameInput").value;
     var email = document.getElementById("emailInput").value;
     var number = document.getElementById("numberInput").value;
-    console.log("name",name);
-    window.alert("name",name);
+    console.log("name:",name);
+    console.log("email:",email);
+    console.log("number:",number);
+    window.alert("name: " + name);
     return false;
 }
