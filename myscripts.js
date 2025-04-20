33752cc7fa1bd7bebc2f68d30ca59154144f36ef
Anh-Tai-Nguyen-J <davidnguyen6150@gmail.com>
2025-04-20 14:34:26 -0700

    david | we added a form, and configure it to accept name, email and number

--- myscripts.js
+++ myscripts.js
@@ -6,6 +6,27 @@ function submitForm(event) {
     console.log("name:",name);
     console.log("email:",email);
     console.log("number:",number);
-    window.alert("name: " + name);
-    return false;
+    if (name === ""){
+        document.getElementById("missingName").style.display= "inline";
+        return false;
+    }
+    else {
+        document.getElementById("missingName").style.display= "none";
+    }
+    if (email === "" || email.indexOf("@") < 0){
+        document.getElementById("missingEmail").style.display= "inline";
+        return false;
+    }
+    else {
+        document.getElementById("missingEmail").style.display= "none";
+    }
+    if (number === ""){
+        document.getElementById("missingNumber").style.display= "inline";
+        return false;
+    }
+    else {
+        document.getElementById("missingNumber").style.display= "none";
+    }
+    console.log("form is valid");
+    return true;
 }
